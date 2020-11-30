1a8681a50c4967a56a1c17082a8f1aeaa08d5451
Furio Alessio <alefurio@hotmail.it>
2020-11-30 13:17:20 +0100

    header + main section

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+var app = new Vue({
+	el: '#root',
+
+	data: {
+		navMenu: ['Home', 'Pages', 'Courses', 'Features', 'Blog', 'Shop'],
+	},
+
+	methods: {
+
+
+	}, // fine methods
+
+	mounted() {
+
+	} // fine mounted
+});
